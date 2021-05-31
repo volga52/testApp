bad69074fe972fe7ce75d69d928e21dc14903f4c
volga52 <volga52@bk.r>
2021-05-31 16:28:15 +0300

    Vue testapp

---
+++ src/components/DataComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        content
+        <input v-model="inputValue">
+        {{ inputValue.toUpperCase() }}
+        <hr>
+    </div>
+</template>
+
+<script>
+export default{
+  data: () => ({
+      inputValue: ''
+  }),
+  watch: {
+      inputValue() {
+          if (this.inputValue.length > 9) {
+              console.log('Слишком длинное слово');
+          }
+      }
+  }
+}
+</script>
