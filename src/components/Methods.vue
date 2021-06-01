42dcd276eb7956ecbc713b8f49bf26152f6a7dca
volga52 <volga52@bk.ru>
2021-06-01 12:29:11 +0300

    Commit1

---
+++ src/components/Methods.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        {{ status }}
+        <input v-model="firstname" />
+        <br>
+        {{ fullUserName }}
+        <input v-model="lastname" />
+        <button @click.once="sendDataToServer">Send</button>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        status: 'not sender',
+        firstname: "",
+        lastname: "",
+    }),
+    methods: {
+        sendDataToServer() {
+            const sendUp = {
+                firstname: this.firstname,
+                lastname: this.lastname,
+            };
+
+            console.log(sendUp);
+            this.status = 'sended';
+        },
+    },
+    computed: {
+        fullUserName() {
+            return `${this.firstname.toUpperCase()} ${this.lastname.toUpperCase()}`;
+        },
+    },
+};
+</script>
